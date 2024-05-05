cef03444a55d4b7da0fee62099deaeeedca646a8
Rafael Becerra Guerra <rafikibece9@gmail.com>
2024-05-05 21:54:49 +0200

    Modificacion ControladorTerapeuta 02

--- src/main/kotlin/logica/ControladorTerapeuta.kt
+++ src/main/kotlin/logica/ControladorTerapeuta.kt
@@ -38,30 +38,31 @@ class ControladorTerapeuta () {
             return TerapeuraDAO.updateTerapeuta(usuario)
         }
 
-        fun cambiarGrupo(email: String, nuevoGrup:String ) {
+        fun cambiarGrupo(email: String, nuevoGrup: String) {
             val usuario = TerapeuraDAO.getTerapeutaByEmail(email)
             usuario?.let {
                 val grupEnum = when (nuevoGrup.uppercase(Locale.getDefault())) {
-                    "GRUPO1" ->  Grupo.GRUPO1
+                    "GRUPO1" -> Grupo.GRUPO1
                     "GRUPO2" -> Grupo.GRUPO2
                     "GRUPO3" -> Grupo.GRUPO3
                     else -> throw IllegalArgumentException("Metodologia desconocido: $nuevoGrup")
                 }
                 it.id_grupos = grupEnum
                 TerapeuraDAO.updateGrupo(usuario)
-        }
+            }
 
-        fun cambiarMetodo(email: String, nuevoMeto:String) {
-            val usuario = TerapeuraDAO.getTerapeutaByEmail(email)
-            usuario?.let {
-                val metoEnum = when (nuevoMeto.uppercase(Locale.getDefault())) {
-                    "METO1" ->  Metodologia.METO1
-                    "METO2" ->  Metodologia.METO2
-                    "METO3" -> Metodologia.METO3
-                    else -> throw IllegalArgumentException("Metodologia desconocido: $nuevoMeto")
+            fun cambiarMetodo(email: String, nuevoMeto: String) {
+                val usuario = TerapeuraDAO.getTerapeutaByEmail(email)
+                usuario?.let {
+                    val metoEnum = when (nuevoMeto.uppercase(Locale.getDefault())) {
+                        "METO1" -> Metodologia.METO1
+                        "METO2" -> Metodologia.METO2
+                        "METO3" -> Metodologia.METO3
+                        else -> throw IllegalArgumentException("Metodologia desconocido: $nuevoMeto")
+                    }
+                    it.id_metodologia = metoEnum
+                    TerapeuraDAO.updateMetodo(usuario)
                 }
-                it.id_metodologia = metoEnum
-               TerapeuraDAO.updateMetodo(usuario)
             }
         }
     }
