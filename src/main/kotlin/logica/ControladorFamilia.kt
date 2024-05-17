06b669516a2bb6c22bff49df74444330bf306310
Rafael Becerra Guerra <rafikibece9@gmail.com>
2024-05-17 11:19:31 +0200

    creacion de clase familia 02

--- src/main/kotlin/logica/ControladorFamilia.kt
+++ src/main/kotlin/logica/ControladorFamilia.kt
@@ -1,4 +1,23 @@
 package logica
 
+import BBDD.FamiliaDAOImpl
+import Familia
+
 class ControladorFamilia {
+    private val familiaDAO = FamiliaDAOImpl()
+    fun obtenerfamilias(): List<Familia> {
+        return familiaDAO.getAllFamilias()
+    }
+    fun eliminarfamilia(familia: Familia): Boolean {
+        return familiaDAO.deleteFamilia(familia)
+    }
+
+    fun añadirfamilia(descripcion:String) {
+        val familiaNueva = Familia(descripcion)
+        familiaDAO.insertFamilia(familiaNueva)
+    }
+
+    fun actualizarfamilia(familia: Familia): Boolean {
+        return familiaDAO.updateFamilia(familia)
+    }
 }
