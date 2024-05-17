d5f7efda62cb5c0cf6e5cf84eff80eeedb83f86d
Rafael Becerra Guerra <rafikibece9@gmail.com>
2024-05-17 11:19:31 +0200

    creacion de clase familia 02

--- src/main/kotlin/BBDD/FamiliaDAOImpl.kt
+++ src/main/kotlin/BBDD/FamiliaDAOImpl.kt
@@ -1,4 +1,56 @@
 package BBDD
 
-class FamiliaDAOImpl {
+import ConexionBD
+import Familia
+
+class FamiliaDAOImpl:FamiliaDAO {
+    private val conexion = ConexionBD()
+    override fun getAllFamilias(): List<Familia> {
+        conexion.conectar()
+        val query = "SELECT * FROM familia"
+        val st = conexion.getStatement()
+        val rs = st?.executeQuery(query)
+        val familia = arrayListOf<Familia>()
+        while (rs?.next() == true) {
+            familia.add(Familia(rs.getInt("id"), rs.getString("descripcion") ))
+        }
+        st?.close()
+        conexion.desconectar()
+        return familia
+    }
+
+    override fun insertFamilia(familia: Familia): Boolean {
+        conexion.conectar()
+        val query = "INSERT INTO familia (descripcion ) VALUES (?)"
+        val ps = conexion.getPreparedStatement(query)
+        ps?.setString(1, familia.descripcion)
+        val result = ps?.executeUpdate()
+        ps?.close()
+        conexion.desconectar()
+        return result == 1
+    }
+
+    override fun updateFamilia(familia: Familia): Boolean {
+        conexion.conectar()
+        val query = "UPDATE familia SET descipcion = ? WHERE id = ?"
+        val ps = conexion.getPreparedStatement(query)
+        ps?.setString(1, familia.descripcion)
+        ps?.setInt(2, familia.id)
+        val result = ps?.executeUpdate()
+        ps?.close()
+        conexion.desconectar()
+        return result == 1
+    }
+
+    override fun deleteFamilia(familia: Familia): Boolean {
+        conexion.conectar()
+        val query = "DELETE FROM familia WHERE id = ?"
+        val ps = conexion.getPreparedStatement(query)
+        ps?.setInt(1, familia.id)
+        val result = ps?.executeUpdate()
+        ps?.close()
+        conexion.desconectar()
+        return result == 1
+    }
+
 }
